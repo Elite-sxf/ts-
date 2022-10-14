6f8caee71c329e3a2e2c37398e47a11a6fae9aca
Elite-sxf <1399826164@qq.com>
2022-10-14 19:36:06 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <span>{{msg}}</span>
+    <span>{{num}}</span>
+    <span>{{flag ? "是":"否"}}</span>
+
+    <ul>
+      <li v-for="i in arr" :key="i">{{i}}</li>
+    </ul>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const msg:String = "这是一个字符串";
+const num:Number = 0;
+const flag:Boolean = false;
+const arr: Array<number> = [0,1,2,3,4];
+
+
+</script>
+
+
+<style scoped>
+*{
+  list-style: none;
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
