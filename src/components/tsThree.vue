d7f5bfcd741061bc6e460ed9d58c283bcecc5f98
Elite-sxf <1399826164@qq.com>
2022-10-21 18:53:52 +0800

    '装饰器'

--- src/components/tsThree.vue
+++ src/components/tsThree.vue
@@ -43,6 +43,48 @@ const Person = testTs()(class {
 })
 let p = new Person("飞哥");
 p.show()
+
+//属性装饰器
+function nameDesc(target:any,key:string) {
+
+}
+class nameTesc {
+    @nameDesc
+    name = "我是谁";
+}
+let n = new nameTesc();
+n.name = 'asd';
+
+//装饰器实例  貌似是一个提出来的公共方法
+const info:any = undefined;
+
+function methodDecorator (msg:string) {
+    return function (target:any, key: string , descriptor: PropertyDescriptor) {
+        console.log(descriptor);
+        const fn = descriptor.value;
+        descriptor.value = function() {
+            try {
+                fn()
+            } catch (error) {
+                console.log(msg);
+                
+            }
+        }
+    }
+}
+class exMethod {
+    @methodDecorator('没有name')
+    getName() {
+        return info.name
+    }
+    @methodDecorator('age')
+    getAge() {
+        return info.age
+    }
+}
+const e = new exMethod();
+e.getName();
+e.getAge();
 </script>
 
 <style>
